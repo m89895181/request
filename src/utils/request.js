f47596e6b9d16f13247bf93b9f0de2d2a17be4b6
destiny <2425052816@qq.com>
2019-09-04 11:09:13 +0800

    请求拦截配置完成 但是没有配置登录组件 只有简单的文字

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,17 +2,26 @@
  * @Author: 苑振东 
  * @Date: 2019-09-03 16:49:53 
  * @Last Modified by: 苑振东
- * @Last Modified time: 2019-09-03 16:56:19
+ * @Last Modified time: 2019-09-04 11:00:22
  */
 import axios from 'axios'
-
+import router from '@/router'
 // 创建axios实例
 let instance = axios.create();
 
 // 添加请求拦截器(request)
 instance.interceptors.request.use(function(config) {
+    // 是不是登录和注册的接口 (不需要token的)
+    let notTokenUrl = ['/api/login', '/api/registry'];
     // 在发送请求之前做些什么
-    return config
+    return notTokenUrl.includes(config.url) ? config : {
+        ...config,
+        headers: {
+            ...config.headers,
+            // 在登陆成功存储的token 在这里获取
+            token: window.localStorage.getItem('token')
+        }
+    }
 }, function(error) {
     // 对错误请求做些什么
     return Promise.reject(error)
@@ -22,6 +31,28 @@ instance.interceptors.request.use(function(config) {
 instance.interceptors.response.use(function(response) {
     return response
 }, function(error) {
+    let { status } = error.response;
+
+    if (status < 500) {
+        switch (status) {
+            case 422:
+                {
+                    alert('缺少参数 数据传递有误')
+                    return
+                }
+            case 401:
+                {
+                    alert('权限不够 登陆过期')
+                    router.push('/login')
+                    return
+                }
+            default:
+                break;
+        }
+    } else {
+        alert('服务端错误')
+    }
+
     // 对错误响应做点什么
     return Promise.reject(error)
 })
